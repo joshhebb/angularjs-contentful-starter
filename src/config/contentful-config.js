1b8bf7f74b5a542ab9f08fc2a30c8ca945042545
Josh Hebb <jfhebb@mta.ca>
2018-02-08 00:12:36 -0400

    Update contentful-config.js

--- src/config/contentful-config.js
+++ src/config/contentful-config.js
@@ -1,3 +1,3 @@
 angular.module("angular-contentful-starter")
-.constant("spaceId", "1kvdu1q179xe")
-.constant("accessToken", "78d6a1f162e47f963778ef668079392e9cbf13ab214a44c05c883c1920b65e35");
+.constant("spaceId", "")
+.constant("accessToken", "");
